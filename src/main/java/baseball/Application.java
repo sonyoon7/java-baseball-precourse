a12dc37975c78e54220b4848d5e6ced4631c0f20
sonyoon7 <sonyoon7@gmail.com>
2022-09-27 22:07:35 +0900

    feat(Application): 야구게임 실행 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
 
+import baseball.game.BaseballGame;
+import baseball.utils.Message;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        BaseballGame baseballGame = new BaseballGame();
+        while (true) {
+            baseballGame.play();
+            if (!baseballGame.isNewGame()) {
+                System.out.println(Message.STOP.getMessage());
+                break;
+            }
+        }
     }
 }
